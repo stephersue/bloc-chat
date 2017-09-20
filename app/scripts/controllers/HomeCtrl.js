ca704fcb7df2d127c38bafd51b0db510dd714b3b
stephersue <stephanie.s.strong@gmail.com>
2017-09-19 18:09:20 -0600

    Querying Messages, Checkpoint 4

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,12 @@
 (function() {
-    function HomeCtrl(Room) {
+    function HomeCtrl(Room, Message) {
       this.rooms = Room.all;
+
+      this.activeRoom = null;
+      this.changeRoom = function(room){
+        this.activeRoom = room;
+        this.messages = Message.getByRoomId(this.activeRoom.$id);
+      };
     }
 
     angular
---
+++ app/scripts/services/Message.js
@@ -0,0 +1,29 @@
+(function() {
+  function Message($firebaseArray) {
+    var Message = {};
+    var ref = firebase.database().ref().child("messages");
+    var messages = $firebaseArray(ref);
+
+    Message.getByRoomId = function(roomId) {
+      return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+    }
+
+    Message.send = function(newMessage, currentRoom) {
+      var date = new Date();
+      var post = {
+          username: "<USERNAME HERE>",
+          content: newMessage,
+          sentAt: $filter('date')(date, 'shortTime'),
+          roomId: currentRoom.$id
+      }
+      messages.$add(post);
+      
+    }
+
+    return Message;
+  }
+
+  angular
+    .module('blocChat')
+    .factory('Message', ['$firebaseArray', Message]);
+})();
